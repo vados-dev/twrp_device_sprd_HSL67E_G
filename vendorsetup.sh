88752f98638826630d83a0968f55210f1c12118c
vados-dev <vados@vados.ru>
2025-01-15 07:22:45 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_HSL67E_G-user
-add_lunch_combo omni_HSL67E_G-userdebug
-add_lunch_combo omni_HSL67E_G-eng
+add_lunch_combo omni_twrp_device_sprd_HSL67E_G-user
+add_lunch_combo omni_twrp_device_sprd_HSL67E_G-userdebug
+add_lunch_combo omni_twrp_device_sprd_HSL67E_G-eng
